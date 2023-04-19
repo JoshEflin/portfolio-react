89317025a599bab8ce970c56e9a61118922a3334
ragingpenguin <eflinjh@gmail.com>
2023-04-19 15:19:18 -0400

    moved some CSS properties around

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ReactTyped from 'react-typed'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-       
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-     
-      </div>
+    <div className="container">
+
+      <h1>Josh Eflin: Web Development</h1>
+      <div className="text">
+      <ReactTyped
+        strings={[
+            'Opera Singer turned Web Developer',
+            'Welcome',
+            'Built with React and Typescript'
+            ]}
+            typeSpeed={40}
+            backSpeed={50}
+            loop
+      />
+    </div>
+    <button>Enter</button>
     {/* <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a> */}
