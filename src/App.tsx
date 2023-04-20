114912df51a6cdbb4bee80b2bc1208b355c1e397
ragingpenguin <joshuahuntereflin@gmail.com>
2023-04-20 10:30:14 -0400

    added SVG J

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,16 @@ import './App.css'
 import ReactTyped from 'react-typed'
 import Welcome from './components/welcome'
 import Nav from './components/nav'
+import J from './components/svg'
 
 function App() {
  
 
   return (
     <div className='wrapper'>
-    <Nav />
-    <Welcome />
+      <J />
+      <Nav />
+      <Welcome />
     </div >
   )
 }
