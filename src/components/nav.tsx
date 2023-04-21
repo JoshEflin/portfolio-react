c44eb105bc1b599d4a0ec31d7bce84020cfeb52f
ragingpenguin <joshuahuntereflin@gmail.com>
2023-04-21 00:52:17 -0400

    made nav list in to functional components

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,7 +1,22 @@
 import J from "./svg"
 import { useState } from "react"
 
+const navItemsArr ={data:['About Me', 'Resume', 'Projects', 'Contact'] }
+const NavItem: React.FC<{navLink:string[]}> = ( {navLink} )=>{
+  return(
+    <>
+    {navLink.map((item,index)=>(
+      <li className="nav-list-item" >
+        <span key={index}>
+          {item}
+        </span>
+      </li>
+  ))}
+</>
+  )
+}
 export default function Nav () {
+
  const [clicked, setClicked] =useState(false)
  
  function handleClick(){
@@ -17,18 +32,7 @@ export default function Nav () {
         <nav>
       <ul className='navbar'>
         <J />
-      <li>
-        <span>About Me</span>
-        </li>   
-        <li>
-        <span>Resume</span>
-        </li>         
-        <li>
-        <span>Projects</span>
-        </li>         
-        <li>
-        <span>Contact</span>
-        </li>                
+      <NavItem navLink={navItemsArr.data} />
       </ul>
     </nav>
     )
