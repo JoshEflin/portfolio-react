e5f361218e6b581863817e8d28394b5ec7e4ab63
ragingpenguin <eflinjh@gmail.com>
2023-04-23 02:08:07 -0400

    added some details to footer, and some css

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import gitHubIcon from '../assets/github-mark.png'
+import phoneIcon from '../assets/Phone-Icon-PNG.png'
 export default function Footer(){
 
     return(
         <footer id="footer">             
-        <h3> Click an icon below to contact me!</h3>
+        <h3> </h3>
         
         <section>
             <ul>
@@ -16,12 +17,12 @@ export default function Footer(){
                 </li>
                 <li>
                     <a href="tel: +1-571-216-7020">
-                        <img src="./assets/images/Phone-Icon-PNG.png"/>
+                        <img src={phoneIcon}/>
                     </a>
                 </li>
                 <li>
                     <a href="http://www.github.com/JoshEflin">
-                        <img src="assets/images/github-mark.png"/>
+                        <img src={gitHubIcon}/>
                     </a>
                 </li>
             </ul>
