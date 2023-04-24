e4c49636ccbc4d65171bb6895b4a06a4f62f2026
ragingpenguin <eflinjh@gmail.com>
2023-04-24 18:07:37 -0400

    added email validation, still need to add onblur for homework requirements

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import {useState,ChangeEvent,FormEvent} from "react";
 
 
 export default function ContactForm() {
@@ -6,7 +6,7 @@ export default function ContactForm() {
     const [email, setEmail]= useState('');
     const [text, setText]= useState('')
 
-    const handleInputChange = (e: any) => {
+    const handleInputChange = (e:ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     if (name === 'firstName') {
       setFirstName(value);
@@ -19,20 +19,27 @@ export default function ContactForm() {
 
   // return name ==='firstName'? setFirstName(value): setEmail(value)
     
-  const handleFormSubmit = (e:any) => {
+  const handleFormSubmit = (e:FormEvent<HTMLFormElement>) => {
       // Preventing the default behavior of the form submit (which is to refresh the page)
       e.preventDefault();
       console.log(e)
-      let missing:string= ''
-      if(!firstName||!email||!text){
-        switch(false){
-          case Boolean(firstName):
-            missing+= "First Name";
+      let missing:string[]= []
+      if(!firstName){
+            missing.push("first-name");
+      } if (!email){
+        missing.push("email");
+      } if(!text){
+        missing.push("text");
+      } 
+      alert(`You are missing the following fields:\n${missing[0]}\n${missing[1]||''}\n${missing[2]||''}`)
+      
+      if (email){
+        const emailregex=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        const isValid =emailregex.test(email)
+        if(!isValid){
+          alert('The email you entered is not valid')
         }
-
-        alert(` You are missing a required field!`)
       }
-    
       // Alert the user their first and last name, clear the inputs
       alert(`This is a test feature only, If you'd like to send a real email please click the mail icon in the footer`);
       setFirstName('');
@@ -43,7 +50,7 @@ export default function ContactForm() {
     return (
       <div className="contact-container">
         
-        <form className="contact-form">
+        <form className="contact-form" onSubmit={handleFormSubmit}>
           <input className="inputs name-input"
             value={firstName}
             name="firstName"
@@ -56,16 +63,16 @@ export default function ContactForm() {
             name="email"
             onChange={handleInputChange}
             type="text"
-            placeholder="email"
+            placeholder="Email Address"
           />
           <input className="inputs text-input"
           value={text}
           name="text"
           onChange= {handleInputChange}
           type="text"
-          placeholder="your message here"
+          placeholder="Enter your message here"
           />
-          <button type="button" onClick={handleFormSubmit}>
+          <button type="submit"> 
             Submit
           </button>
         </form>
