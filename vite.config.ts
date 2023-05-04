0ec373ba990b90caf8e3df02fcd749769a825bd8
ragingpenguin <eflinjh@gmail.com>
2023-05-04 02:29:26 -0400

    changing vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/portfolio-react",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('/node_modules/')) {
+            return id
+              .toString()
+              .split('/node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 
   plugins: [react()],
 })
