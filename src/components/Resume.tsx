1728743ff301649052eeb3a23a485ac752c6d68d
ragingpenguin <eflinjh@gmail.com>
2023-08-08 04:55:35 -0400

    fixed resume

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,16 +1,11 @@
 import download from "../assets/JoshEflin-Resume.pdf";
 export default function Resume() {
-  const resumeSrc =
-    '<iframe src="https://docs.google.com/document/d/e/2PACX-1vQB3JreeBWyoSghI53Y1h3fBy2-wxv-nehKha7qlfSJmUswkRbKiOFCx2ydM1UDow/pub?embedded=true"></iframe>';
+  // const resumeSrc =
+  //   '<iframe src="https://docs.google.com/document/d/e/2PACX-1vQB3JreeBWyoSghI53Y1h3fBy2-wxv-nehKha7qlfSJmUswkRbKiOFCx2ydM1UDow/pub?embedded=true"></iframe>';
   return (
     <>
       <h2> Resume and Skills</h2>
-      <iframe
-        id="resume"
-        src={
-          "https://docs.google.com/document/d/e/2PACX-1vQe3rRIt8OqS_CvyEl-_gh2azjktITlmJAr4r4adHol1pgXHRKbvQ41nX0NDmPOOtWbqI-zSU2PhlSR/pub?embedded=true"
-        }
-      ></iframe>
+      <iframe id="resume" src={download}></iframe>
       <a href={download} download>
         <h2>Download resume</h2>
       </a>
